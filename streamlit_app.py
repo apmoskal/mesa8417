6e7a38a6454a3aad4cdbddf95a02d374f52f35b0
apmoskal <moskalap@bc.edu>
2025-06-28 21:32:50 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,7 +72,7 @@ st.altair_chart(hist_chart, use_container_width=True)
 
 #with tab2:
 st.subheader("Listing Locations")
-st.map(filtered[["latitude", "longitude"], color=="neighbourhood_group_cleansed"].dropna())
+st.map(filtered[["latitude", "longitude"].dropna()], color="neighbourhood_group_cleansed")
 
 #with tab3:
 st.subheader("Filtered Listings")
